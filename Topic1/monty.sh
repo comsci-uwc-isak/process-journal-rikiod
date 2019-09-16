b3fe1c67cdd565e42bdfff3384cde756d9918214
Rikio Dahlgren <rikiodahlgren@Rikios-MacBook-Pro.local>
2019-09-16 13:55:09 +0900

    Uploaded saved program

--- Topic1/monty.sh
+++ Topic1/monty.sh
@@ -13,7 +13,7 @@ fi
 one=1
 two=2
 three=3
-ran=(($RANDOM%3)+1)
+ran=$((RANDOM%3)+1)
 echo $ran
 
 array=($one $two $three)
@@ -27,4 +27,31 @@ case $ans in
 		;; 
 esac
 
-echo ${array[@]}
+
+
+
+#!/bin/bash
+#This program will simulate the Monty Hall problem
+prize=random%3+1
+nothing=random%3+1
+
+#Asking the user to pick a door.
+echo "please enter doors"
+read door 
+
+#Calculating which door will have the prize and which will have nothing.
+while nothing=prize;
+	prize=random%3+1
+	nothing=random%3+1
+done
+
+#Asking the user if they'd like to switch doors.
+echo "The $nothing door has nothing in it."
+echo "Would you like to change doors? (Y/N)"
+read ans
+
+if [[ ans=no ]]; then
+		if [[ door= ]]; then
+			#statements
+		fi
+	fi	
