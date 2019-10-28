5f51d29864cd5179340f0aeb989a06f8a3045f58
rikiod <54606933+rikiod@users.noreply.github.com>
2019-10-28 20:03:57 +0900

    fixed program
    realized 21st is not "21th" and fixed program accordingly

--- ForLoopPractice/6.ordinal.sh
+++ ForLoopPractice/6.ordinal.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 #This program prints the ordinal numbers from one to a hundred.
+
+for (( i = 1; i < 101; i++ )); do
+	((lastDigit=$i%10)) #Checking last digit of number
+
+	if [[ ($lastDigit -eq 1) ]]; then
+        	echo $i"st"
+	
+	elif [[ ($lastDigit -eq 2) ]]; then
+       		echo $i"nd"
+   
+   	elif [[ ($lastDigit -eq 3) ]]; then
+		echo $i"rd"
  
-echo -n "1st 2nd 3rd "
-for (( i = 4; i < 100; i++ )); do
-	echo -n $i"th "
+ 	else
+		echo $i"th"
+	
+	fi
 done
